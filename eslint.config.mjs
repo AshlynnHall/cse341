fc1053be985a783a7e4afde78b1fbbc5a6dda307
Ashlynn Hall <hall.ash9@gmail.com>
2025-05-17 17:16:57 -0600

    assignment 3

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import json from "@eslint/json";
+import markdown from "@eslint/markdown";
+import css from "@eslint/css";
+import { defineConfig } from "eslint/config";
+
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js, prettier: require("eslint-plugin-prettier") },
+    extends: ["js/recommended", "plugin:prettier/recommended"],
+    env: {
+      browser: true,
+      es2021: true,
+      jest: true
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  },
+  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
+  pluginReact.configs.flat.recommended,
+  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
+  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
+  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/commonmark", extends: ["markdown/recommended"] },
+  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+]);
